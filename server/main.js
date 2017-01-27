afcb1b9fcec9c31773716f8b8770c3fbc3c4cf5f
Emmanuel Ovando <dmkboo@gmail.com>
2017-01-27 17:27:56 -0600

    added app to github

---
+++ server/main.js
@@ -0,0 +1,38 @@
+import { Meteor } from 'meteor/meteor';
+import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
+
+Meteor.startup(() => {
+  Meteor.publish('links', function (){
+    return Links.find({});
+  });
+});
+
+//executed whenever a user visits with a route like
+// 'localhost:3000/abcd'
+function onRoute(req, res, next) {
+  // Take the token out of the url and try to find a
+  // matching link in the Links Collection
+  const link = Links.findOne({ token: req.params.token })
+
+  if(link) {
+    //If we find a link object, redirect the user to the
+    // long URL
+    Links.update(link, { $inc: { clicks: 1 } });
+    res.writeHead(307, { 'Location': link.url });
+    res.end();
+
+  } else{
+    //if we dont find a link object, send the user
+    // to our normal React app
+    next();
+  }
+
+
+}
+const middleware = ConnectRoute(function(router){
+  router.get('/:token', onRoute);
+});
+
+WebApp.connectHandlers.use(middleware);
